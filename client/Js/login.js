6cd21819934c6c406d97e8bc8ab693b3adb9747b
sonu101994 <sonu10211995@gmail.com>
2026-05-10 10:24:29 +0530

    created two functions two login and register user and store token in session storage

---
+++ client/Js/login.js
@@ -0,0 +1,115 @@
+//================= LOGIN FUNCTION =========================
+
+const login=async()=> {
+
+    // Get input values
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    // Send login request using apiRequest()
+    const data = await apiRequest(
+        "/auth/login",
+        "POST",
+        { email, password }
+    );
+
+    // Check login success
+    if (data.token) {
+
+        // Store user data in session
+        sessionStorage.setItem("token", data.token);
+        sessionStorage.setItem("userId", data.user._id);
+        sessionStorage.setItem("role", data.user.role);
+        sessionStorage.setItem("username", data.user.username);
+        // Redirect to dashboard
+        window.location.href = "dashboard.html";
+
+    } else {
+
+        // Show error
+        showToast(data.message || "Login failed ❌");
+    }
+}
+
+// ================= FUNCTIO TO REGISTER USER =================
+ const register=async()=> {
+
+    // Get form values
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    // Validate empty fields
+    if (!username || !email || !password || !confirmPassword) {
+        showToast("All fields required ❗");
+        return;
+    }
+
+    // Check password match
+    if (password !== confirmPassword) {
+        showToast("Passwords do not match ❌");
+        return;
+    }
+
+    // Send register request using apiRequest()
+    const data = await apiRequest(
+        "/auth/register",
+        "POST",
+        { username, email, password }
+    );
+
+    console.log(data)
+    // Success check
+    if (data.message === "User registered successfully") {
+
+        showToast("Registered successfully ✅");
+        
+
+        // Clear form fields
+        document.getElementById("username").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+        document.getElementById("confirmPassword").value = "";
+
+        setTimeout(() => {
+            window.location.href="index.html";
+        }, 1000);
+
+    } else {
+
+        // Error message
+        showToast(data.message || "Error ❌");
+    }
+}
+
+// ================= TOGGLE PASSWORD =================
+function togglePassword(id) {
+
+    // Get password input
+    const input = document.getElementById(id);
+
+    // Toggle type
+    input.type =
+        input.type === "password"
+            ? "text"
+            : "password";
+}
+
+// ================= SHOW TOAST =================
+function showToast(msg) {
+
+    // Get toast element
+    const toast = document.getElementById("toast");
+
+    // Set message
+    toast.innerText = msg;
+
+    // Show toast
+    toast.classList.add("toast-show");
+
+    // Hide after 2 sec
+    setTimeout(() => {
+        toast.classList.remove("toast-show");
+    }, 2000);
+}
